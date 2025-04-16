cfeefc6591c4763476e565ae76632ba2ce5643f6
adityamakadiya <aditya.makadiya@bacancy.com>
2025-04-16 14:53:39 +0530

    Update App.jsx

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -3,6 +3,8 @@ import EmployeeDashboard from './Components/EmployeeDashboard';
 import Login from './Components/Login';
 import Register from './Components/Register';
 import { BrowserRouter , Route, Routes } from 'react-router-dom';
+import AdminPage from './pages/AdminPage';
+import TaskForm from './Components/TaskForm';
 
 function App() {
   return (
@@ -11,8 +13,8 @@ function App() {
         <Route path="/" element={<EmployeeDashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/employee" element={<EmployeeDashboard />} />
-        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/employee" element={<EmployeeDashboard/>} />
+        <Route path="/admin" element={<AdminPage/>} />
         <Route path="/admin/create-task" element={<TaskForm />} />
       </Routes>
     </BrowserRouter>
