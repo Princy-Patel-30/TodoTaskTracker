988ecadd138e66bc23de6451af1a74db1138c135
adityamakadiya <aditya.makadiya@bacancy.com>
2025-04-16 14:00:17 +0530

    [FEAT] : Store_Setup

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import EmployeeDashboard from './Components/EmployeeDashboard'
 
--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,8 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./Store/store";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-)
+  </Provider>
+);
